c1728975423545f72f88081d6d109237c264ab41
Hossain Nur <nurhossainsani1@gmail.com>
2024-06-01 22:46:23 +0600

    input added

---
+++ src/main/kotlin/Input.kt
@@ -0,0 +1,7 @@
+fun main(){
+    print("Enter Your Name: ")
+    val name : String? = readLine()
+    val number: String? = readLine()
+    println("Your Name is: $name")
+    println("Number is: ${number.toString()}")
+}
