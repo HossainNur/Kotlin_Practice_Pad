e5176c917c2a66260e29d1fae42365743463eecf
HossainNur <nurhossainsani1@gmail.com>
2024-06-03 15:05:21 +0600

    large number check added

---
+++ src/main/kotlin/LargestNumber.kt
@@ -0,0 +1,19 @@
+import java.util.*
+
+fun main(){
+    val reader = Scanner(System.`in`)
+    print("Enter three numbers: ")
+    var num1 = reader.nextInt()
+    var num2 = reader.nextInt()
+    var num3 = reader.nextInt()
+
+    var max = if (num1 > num2) {
+        if (num1 > num3){
+            "$num1 is the largest number"
+        }else "$num3 is the largest number"
+    }else if (num2 > num3){
+        "$num2 is the largest number"
+    }else "$num3 is the largest number"
+
+    println("$max")
+}
