4dbf4a667499dc4de76ef3f07e60356271355760
HossainNur <nurhossainsani1@gmail.com>
2024-06-24 15:48:04 +0600

    updated: factorial added

--- src/main/kotlin/Function.kt
+++ src/main/kotlin/Function.kt
@@ -3,9 +3,11 @@ fun main(){
     //myFunction("George",43)
     /*var result = myFunction(3)
     println(result)*/
-    var sum = arrayOf(1,2,3,4,5,6,7,8,9,10).sum()
+    /*var sum = arrayOf(1,2,3,4,5,6,7,8,9,10).sum()
 
-    println("The sum of all the elements of an array is: $sum")
+    println("The sum of all the elements of an array is: $sum")*/
+    val result  = fact(5)
+    println("Factorial of 5 is: $result")
 
 }
 /*
@@ -15,3 +17,8 @@ fun myFunction(fname: String,age: Int){
 }*/
 
 fun myFunction(x:Int): Int = x + 6
+
+fun fact(num: Int): Long {
+    return if (num == 1) num.toLong() // terminate condition
+    else num* fact(num - 1)
+}
