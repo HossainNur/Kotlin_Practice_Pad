a65726adaf64bde570260aa81fb77eaf4b1ae27b
HossainNur <nurhossainsani1@gmail.com>
2024-06-24 13:03:33 +0600

    created: Function.kt

---
+++ src/main/kotlin/Function.kt
@@ -0,0 +1,8 @@
+//A function is a block of code which only runs when it is called.
+fun main(){
+    myFunction("George",43)
+}
+fun myFunction(fname: String,age: Int){
+    //println("I just got executed!")
+    println("$fname is $age")
+}
