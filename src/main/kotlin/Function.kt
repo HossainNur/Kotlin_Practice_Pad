ebc52d04e6ee4be05233585d020187becf91bb0a
HossainNur <nurhossainsani1@gmail.com>
2024-06-24 13:22:22 +0600

    updated: return function

--- src/main/kotlin/Function.kt
+++ src/main/kotlin/Function.kt
@@ -1,8 +1,16 @@
 //A function is a block of code which only runs when it is called.
 fun main(){
-    myFunction("George",43)
+    //myFunction("George",43)
+    var result = myFunction(3)
+    println(result)
+
 }
+/*
 fun myFunction(fname: String,age: Int){
     //println("I just got executed!")
     println("$fname is $age")
+}*/
+
+fun myFunction(x:Int): Int{
+    return (x+5)
 }
