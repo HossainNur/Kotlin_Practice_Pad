98e5b5d9f63e18b45e214c45b2c0256c2db33426
HossainNur <nurhossainsani1@gmail.com>
2024-06-25 16:30:45 +0600

    updated: continue added

--- src/main/kotlin/ForLoop.kt
+++ src/main/kotlin/ForLoop.kt
@@ -3,4 +3,14 @@ fun main(){
     for (car in cars){
         println(car)
     }
+
+    var i = 0
+    while (i < 10) {
+        if (i == 4) {
+            i++
+            continue
+        }
+        println(i)
+        i++
+    }
 }
