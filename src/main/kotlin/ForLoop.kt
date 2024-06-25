9921df05bced92a8086b298954fc175c8d47a2c6
HossainNur <nurhossainsani1@gmail.com>
2024-06-25 16:31:18 +0600

    updated: break keyword added

--- src/main/kotlin/ForLoop.kt
+++ src/main/kotlin/ForLoop.kt
@@ -1,5 +1,5 @@
 fun main(){
-    var cars = arrayOf("Volvo","BMW", "Ford", "Mazda")
+   /* var cars = arrayOf("Volvo","BMW", "Ford", "Mazda")
     for (car in cars){
         println(car)
     }
@@ -12,5 +12,13 @@ fun main(){
         }
         println(i)
         i++
+    }*/
+    var i = 0
+    while (i < 10) {
+        println(i)
+        i++
+        if (i == 4) {
+            break
+        }
     }
 }
