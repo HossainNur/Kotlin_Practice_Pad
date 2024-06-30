e6aeb958ad4e4e5bfcc8e17fe17a7f97b445afd4
HossainNur <nurhossainsani1@gmail.com>
2024-06-30 16:54:14 +0600

    Map Interface mapOf().containsKey()

--- src/main/kotlin/Map.kt
+++ src/main/kotlin/Map.kt
@@ -1,7 +1,16 @@
 fun main(args: Array<String>){
 
-    val myMap = mapOf<Int,String>(1 to "Ajay", 4 to "Vijay", 3 to "Prakash")
+    /*val myMap = mapOf<Int,String>(1 to "Ajay", 4 to "Vijay", 3 to "Prakash")
     for(key in myMap.keys){
         println(myMap[key])
+    }*/
+
+    val myMap: Map<Int,String> = mapOf<Int, String>(1 to "Ajay", 4 to "Vijay", 3 to "Prakash")
+
+    for(key in myMap.keys){
+        println("Element at key $key = ${myMap.get(key)}")
     }
+
+    println("......myMap.containsKey(2)......")
+    println(myMap.containsKey(2))
 }
