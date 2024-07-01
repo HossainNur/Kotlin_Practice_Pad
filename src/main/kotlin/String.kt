b686bad2d68b2898dee92ca76a48e9162c3ceb9a
HossainNur <nurhossainsani1@gmail.com>
2024-07-01 11:08:46 +0600

    check string blank or not

--- src/main/kotlin/String.kt
+++ src/main/kotlin/String.kt
@@ -1,5 +1,10 @@
-fun main(){
-var txt = "Hello World"
-println(txt[0]) // first element (H)
-println(txt[2]) // third element (l)
+fun main() {
+    var txt = "Hello World"
+    println(txt[0]) // first element (H)
+    println(txt[2]) // third element (l)
+
+    val myCountry: String = " "
+    val result: Boolean = myCountry.isBlank()
+    println(result)
+
 }
