f504bc3c6ef66a7793b0339a3dae770c54edc4b9
NurHossain9 <nur.hossain@durbar.live>
2024-07-14 22:42:21 +0600

    updated: find value of sets

--- src/main/kotlin/Sets.kt
+++ src/main/kotlin/Sets.kt
@@ -1,7 +1,11 @@
 fun main(){
     val newSet = setOf("one", "two", "three", "four")
-    println(newSet)
+    /*println(newSet)
 
     val newMutableSet = mutableSetOf("one", "two", "three", "four")
-    println(newMutableSet)
+    println(newMutableSet)*/
+
+    for (i in newSet.indices) {
+        println(newSet.elementAt(i))
+    }
 }
