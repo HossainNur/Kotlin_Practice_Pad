e5b7ea590009ebdc5e1b837736bdc1de9f3ffbae
NurHossain9 <nur.hossain@durbar.live>
2024-07-14 22:41:05 +0600

    created: Sets.kt

---
+++ src/main/kotlin/Sets.kt
@@ -0,0 +1,7 @@
+fun main(){
+    val newSet = setOf("one", "two", "three", "four")
+    println(newSet)
+
+    val newMutableSet = mutableSetOf("one", "two", "three", "four")
+    println(newMutableSet)
+}
